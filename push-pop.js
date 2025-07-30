21fc6f8339942189bafa7869ba770348560c07d3
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-30 23:06:31 +0600

    push pop

---
+++ push-pop.js
@@ -0,0 +1,37 @@
+const numbers = [12, 22, 54, 65, 78, 90, 100, 34, 56, 9, 10];
+
+console.log(numbers);
+
+
+// push
+// numbers.push(55);
+// numbers.push(66);
+// numbers.push(77, 444, 555, 666);
+// console.log(numbers);
+
+// const friends = ['rahim', 'karim', 'jorina', 'sabila', 'sabnur'];
+// console.log(friends);
+// friends.push('abul');
+// console.log(friends);
+
+// pop 
+
+// const friends = ['rahim', 'karim', 'jorina', 'sabila', 'sabnur'];
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+
+
+const friends = ['rahim', 'karim', 'jorina', 'sabila', 'sabnur'];
+console.log(friends);
+const out1 = friends.pop();
+console.log(friends);
+const out2 = friends.pop();
+console.log(friends);
+console.log(out1, out2);
