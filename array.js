2ef6738c598d3a5f96e239fcac4d164c276446e9
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-30 22:41:14 +0600

    Array

---
+++ array.js
@@ -0,0 +1,21 @@
+//  const numbers = [12, 22, 54, 65];
+//  const friends = ['abul', 'babul', 'cabul', 'dabul', 'bulbul', 'kabul'];
+//  const passed = [true, false, true, false, true, false];
+
+// length
+// const numbers = [12, 22, 54, 65, 78, 90, 100, 34, 56, 1, 4, 5, 6, 7, 8, 9, 10];
+// console.log(numbers.length)
+
+//index
+const numbers = [12, 22, 54, 65, 78, 90, 100, 34, 56, 9, 10];
+
+// console.log(numbers[2]);
+// const fourth = numbers[3];
+// console.log(fourth);
+
+numbers[2] = 155;
+console.log(numbers);
+
+const welcome = ['W','E','L','C','O','M','E'];
+console.log(welcome)
+
