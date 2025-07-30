417e3f134b867567deb927657b70c3df5d11ac37
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-30 23:38:53 +0600

    includes

---
+++ includes.js
@@ -0,0 +1,11 @@
+const friends = ['rahim', 'karim', 'jorina', 'sabila', 'sabnur'];
+
+console.log(friends.includes('sabila'))
+console.log(friends.includes('balbal'))
+
+if(friends.includes("rahim")){
+    console.log("parti korbo")
+}
+else{
+    console.log("parti korbo na")
+}
