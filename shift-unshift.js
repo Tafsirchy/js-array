e4db8fc04ec343b2d3a51f1e8adb3c49c6242f95
Tafsirchy <tafsirchy1000@gmail.com>
2025-07-30 23:14:28 +0600

    shift-unshift

---
+++ shift-unshift.js
@@ -0,0 +1,10 @@
+const friends = ['rahim', 'karim', 'jorina', 'sabila', 'sabnur'];
+
+// delete from first position
+console.log(friends);
+friends.shift();
+console.log(friends);
+
+// add in first position
+friends.unshift('jorina');
+console.log(friends);
