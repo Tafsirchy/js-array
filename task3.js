15e669de32d3a640c35d4de98a0768729910f497
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-02 13:12:39 +0600

    task3

---
+++ task3.js
@@ -0,0 +1,9 @@
+let book = ['c','c++','python','javascript'];
+
+// console.log(book.includes('javascript'));
+if(book.includes('javascript')){
+    console.log('This array contains a javascript book')
+}
+else{
+    console.log('This array does not contain a javascript book')
+}
