c8bab748161056d71383801d1f0ab6ca61462a81
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-03 10:03:53 +0600

    task4.js

---
+++ task4.js
@@ -0,0 +1,16 @@
+const fruits = ['apple', 'banana', 'mango', 'orange', 'kiwi', 'cherry'];
+const number = [12, 22, 54, 65, 78, 90, 100, 34, 56, 9, 10];
+const mixed = [12, 'apple', 'banana', true, undefined, null];
+
+const result=[];
+
+const text = 'I am a web developer';
+const point = 5.00;
+
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(number));
+console.log(Array.isArray(mixed));
+console.log(Array.isArray(result));
+console.log(Array.isArray(text));
+console.log(Array.isArray(point));
+
