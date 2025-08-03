7cd88c745ff9453214b76406366dbf333b8a56a5
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-03 10:09:50 +0600

    task5

---
+++ task5.js
@@ -0,0 +1,7 @@
+const family = ['Ammu','Farhana','Arafat','Farhad','Asif','Tauhid','Tamjid','Tafsir'];
+console.log(family);
+const friends = ['Hamim','Tanim','Maruf','Mohi','Arafat','Shohag'];
+console.log(friends);
+
+const wellWishers = family.concat(friends);
+console.log(wellWishers);
